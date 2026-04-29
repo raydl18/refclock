81d4213d915fac5dd1bd70a77ec0e7a96b87c7f4
raydl18 <rayrayliu2007@gmail.com>
2026-04-29 19:21:22 -0400

    update service worker for updates to hit

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'refclock-v5';
+const CACHE = 'refclock-v6';
 const ASSETS = ['/refclock', '/refclock/index.html', '/refclock/style.css', '/refclock/app.js', '/refclock/manifest.json', '/refclock/icons/icon-192.png', '/refclock/icons/icon-512.png'];
 
 self.addEventListener('install', e => {
