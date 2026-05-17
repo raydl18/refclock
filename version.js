d8da868c7c177416f73db317c7879b05ebd6a40a
raydl18 <rayrayliu2007@gmail.com>
2026-05-17 10:21:51 -0700

    created version.js with APP_VERSION and CHANGELOG, exposed following global-namespace pattern

---
+++ version.js
@@ -0,0 +1,28 @@
+const APP_VERSION = '1.2.0';
+const CHANGELOG = [
+    {
+        version: '1.2.0',
+        date: '2026-05-17',
+        changes: [
+            'Added a help section, displaying account deletion option, version number, and changelog'
+        ]
+    },
+    {
+        version: '1.1.0',
+        date: '2026-05-07',
+        changes: [
+            'Added account deletion url, updated privacy policy, improved overall app appearance',
+            'Made game saving optional: requires a button'
+        ]
+    },
+    {
+        version: '1.0.0',
+        date: '2026-04-21',
+        changes: [
+            'Created basic app functionality with timer, goals, cards',
+            'Features: add/subtract time during game, per-half timer, functioning timer on phone close, notes field, login option to save games with history screen'
+        ],
+    },
+];
+
+window.RefClockVersion = { APP_VERSION, CHANGELOG };
